42f46e35a163f175df7d6487076fc20df0f8afa8
DoomGuySoldier <helion7536@gmail.com>
2023-01-11 16:54:47 +0100

    super Mario Bros

--- gol/livingCreatures.js
+++ gol/livingCreatures.js
@@ -1,6 +1,48 @@
 class livingCreatures{
-    constructor(x, y) {
+    constructor(x, y ) {
         this.x = x;
         this.y = y;
+        this.neighbors = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ]
+    }
+
+    updateNeighbors() {
+        this.neighbors = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+
+    chooseCell(symbol) {
+        let found = [];
+        // wir suchen nach leeren Feldern - Wert 0
+        for (let i = 0; i < this.neighbors.length; i++) {
+            const pos = this.neighbors[i];  //[x,y]
+            let posX = pos[0];
+            let posY = pos[1];
+
+            if (posX >= 0 && posX < matrix[0].length && posY >= 0 && posY < matrix.length) {
+                let wert = matrix[posY][posX];
+                if (wert == symbol) {
+                    found.push(pos);
+                }
+            }
+
+        }
+        return found;
     }
 }
