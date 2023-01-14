e1868ae2957c283d3a5a97eaf1102626af66ed15
DoomGuySoldier <helion7536@gmail.com>
2023-01-14 11:08:36 +0100

    MORE

--- session_3/index.js
+++ session_3/index.js
@@ -1,6 +1,12 @@
 const express = require("express");
 const app = express();
 
+app.use(express.static("../gol"));
+
+app.get("/game", function(req, resp){
+    resp.redirect("/index.html");
+})
+
 app.get("/", function(req, resp){
     resp.send("Target locked");
 });
@@ -10,6 +16,15 @@ app.get("/name/:name", function(req, resp){
     resp.send("<h1>Hello " + name +"</h1>");
  });
 
+app.get("/google/:search", function(req, resp){
+    let search = req.params.search;
+    resp.redirect("https://google.com/search?q=" + search);
+})
+
+app.get("/*", function(req, resp){
+    resp.status(404).send("<h1> Error 404 </h1>");
+})
+
 app.listen(3000, function(){
     console.log("Terminator activated");
-}); 
+});
